4b945c03adef33870ed7b13152783007e677744d
latinoamericano <acuariocel@gmail.com>
2016-03-19 08:06:16 -0500

    Creación del proyecto y de la clase Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diego
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido");
+    }
+
+}
