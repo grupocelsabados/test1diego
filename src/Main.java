d229f7335475b5e89eb0f437666b2627e3df8acf
josue291095 <josue291095@gmail.com>
2016-03-19 08:49:20 -0500

    Cambio de nombre de la clase y se agrego versión.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
  *
  * @author Diego
  * @fecha 2016-03-19
+ * @version 1.0
  */
 public class Main {
 
--- src/Test1.java
+++ src/Test1.java
@@ -5,8 +5,9 @@ import javax.swing.JOptionPane;
  *
  * @author Diego
  * @fecha 2016-03-19
+ * @version 1.0
  */
-public class Test {
+public class Test1 {
 
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Test");
